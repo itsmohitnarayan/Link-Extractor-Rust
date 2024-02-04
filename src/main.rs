bb83da477b26a516e0159e7fe523e3fafe6c5de0
Kumar Mohit <mohit724196@gmail.com>
2024-02-04 17:27:05 +0530

    code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use error_chain::error_chain;
+use select::document::Document;
+use select::predicate::Name;
+
+error_chain!{
+    foreign_links{
+        RegError(reqwest::Error);
+        IoError(std::io::Error);
+    }
 }
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let res = reqwest::get("https://www.rust-lang.org/en-US/")
+      .await?
+      .text()
+      .await?;
+  
+    
+  }
